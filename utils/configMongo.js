a3a0433fe9ebd84df72ed2b64e7c4fa30e7c9400
suecollarte <suecollarte@gmail.com>
2022-11-15 17:50:34 -0300

    Add files via upload

--- utils/configMongo.js
+++ utils/configMongo.js
@@ -1,1 +1,14 @@
+ export const config ={
+    db:{
+        host:'localhost',
+        port: 27017,
+        dbName: 'ecommerce',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000
+        }
 
+    }
+
+}

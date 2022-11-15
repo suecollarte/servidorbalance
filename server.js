cc5a2630b3e2d4da99c391290dc3a56c6ca2c161
suecollarte <suecollarte@gmail.com>
2022-11-15 17:49:32 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,187 @@
+/*============================[Modulos]============================*/
+import express from "express";
+import session from "express-session";
+import exphbs from 'express-handlebars';
+import path from 'path';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { userModel } from './models/usuarios.model.js';
+import { config } from './utils/configMongo.js';
 
+dotenv.config();
+
+import passport from "passport";
+import { Strategy } from "passport-local";
+const LocalStrategy = Strategy;
+
+const app = express();
+/*============================[Middlewares]============================*/
+
+
+/*----------- Passport -----------*/
+
+/*
+    SOLUCION: Passport LocalStrategy, utiliza dos valores esperados llamados username y password, por lo que
+    dentro del formulario 'login' debe contener estos dos imputs con el su respectivo nombre.
+
+    En clase no se reconocia el login porque teniamos nombre/password en lugar de username/password en el HTML.
+*/
+
+passport.use(new LocalStrategy(
+    async function(username, password, done) {
+        console.log(`${username} ${password}`)
+        const strConn = `mongodb://${config.db.host}:${config.db.port}/${config.db.dbName}`
+        const conn = await mongoose.connect(strConn, config.db.options);
+        //Logica para validar si un usuario existe
+        //const existeUsuario = await usuariosDB.find(usuario => usuario.username == username);
+        const existeUsuario = await userModel.find(usuario => usuario.username == username);
+        console.log(existeUsuario);
+
+        if (!existeUsuario) {
+            return done(null, false);
+        } else {
+
+            const match = await verifyPass(existeUsuario, password);
+            if(!match){
+                return done(null, false);
+            }
+
+            return done(null, existeUsuario);
+        }
+        await mongoose.disconnect();
+        console.log('Base de datos desconectada!');
+    }
+));
+//correlacionar con los datos
+passport.serializeUser((username, done)=>{
+    done(null, usuario.username);
+});
+
+passport.deserializeUser((username, done)=>{
+    
+ //  const existeUsuario = usuariosDB.find(usuario => usuario.username == username);
+    const strConn = `mongodb://${config.db.host}:${config.db.port}/${config.db.dbName}`
+    const conn =  mongoose.connect(strConn, config.db.options);
+    //Logica para validar si un usuario existe
+    //const existeUsuario = await usuariosDB.find(usuario => usuario.username == username);
+    const existeUsuario =  userModel.find(usuario => usuario.username == username);
+    console.log(existeUsuario);
+    done(null, existeUsuario);
+    mongoose.disconnect();
+});
+
+/*----------- Session -----------*/
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 20000 //20 seg
+    }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Metodos de Auth
+async function generateHashPassword(password){
+    const hashPassword = await bcrypt.hash(password, 10);
+    return hashPassword;
+}
+
+async function verifyPass(usuario, password) {
+    const match = await bcrypt.compare(password, usuario.password);
+    console.log(`pass login: ${password} || pass hash: ${ usuario.password}`)
+    return match;
+}
+
+/*----------- Motor de plantillas -----------*/
+app.set('views', 'src/views');
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+function isAuth(req, res, next) {
+    if(req.isAuthenticated()){
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
+
+/*============================[Base de Datos]============================*/
+//const usuariosDB = [];
+
+/*============================[Rutas]============================*/
+app.get('/', (req, res)=>{
+    res.redirect('/login')
+})
+
+app.get('/login', (req, res)=>{
+    res.render('login.hbs');
+})
+
+app.get('/register', (req, res)=>{
+    res.render('registro.hbs');
+})
+
+app.post('/login', passport.authenticate('local',  {successRedirect: '/datos', failureRedirect: '/login-error'} ));
+
+app.get('/datos', isAuth, (req, res)=>{
+    if(!req.user.contador){
+        req.user.contador = 1
+    } else {
+        req.user.contador++
+    }
+    const datosUsuario = {
+        nombre: req.user.username,
+        direccion: req.user.direccion
+    }
+    res.render('datos', {contador: req.user.contador, datos: datosUsuario});
+})
+
+app.post('/register', async (req, res)=>{
+    const {username, password, direccion } = req.body;
+    const strConn = `mongodb://${config.db.host}:${config.db.port}/${config.db.dbName}`
+    const conn =  mongoose.connect(strConn, config.db.options);
+    //Logica para validar si un usuario existe
+    //const existeUsuario = await usuariosDB.find(usuario => usuario.username == username);
+    const newUsuario =  userModel.find(usuario => usuario.username == username);
+    //const newUsuario = usuariosDB.find(usuario => usuario.username == username);
+    if (newUsuario) {
+        res.render('registro-error')
+    } else {
+        const newUser = {username, password: await generateHashPassword(password), direccion};
+        console.log(newUser);
+        //usuariosDB.push(newUser);
+        await userModel.insertMany(newUser);
+        res.redirect('/login')
+    }
+    await mongoose.disconnect();
+})
+
+app.get('/logout', (req, res)=> {
+    req.logOut(err => {
+        res.redirect('/');
+    });
+})
+
+app.get('/login-error', (req, res)=>{
+    res.render('login-error');
+})
+
+/*============================[Servidor]============================*/
+const PORT = process.env.PORT;
+const server = app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en puerto ${PORT}`);
+})
+server.on('error', error=>{
+    console.error(`Error en el servidor ${error}`);
+});
